7a1c92ad32ab4d38569c8076f3f5480c9f8b419c
Etienne Buffet <etienne.buffet@saintmichelannecy.fr>
2020-09-03 11:05:47 +0200

    Add project files.

---
+++ TPgit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TPgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

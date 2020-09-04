67e027d7ff9ef7955260cac7a9d945182dc1dec2
Etienne Buffet <etienne.buffet@saintmichelannecy.fr>
2020-09-04 10:57:07 +0200

    Avant modification classes métier

--- TPgit/Product.cs
+++ TPgit/Product.cs
@@ -13,6 +13,11 @@ namespace TPgit
         private string provider;
         private string origin;
         private string ecologicalFootprint;
+        private string storageLocation;
+        private string color;
+        private string format;
+        private string quality;
+        private string manager;
 
         public int Id { get => id; set => id = value; }
         public string Name { get => name; set => name = value; }
@@ -20,6 +25,12 @@ namespace TPgit
         public string Provider { get => provider; set => provider = value; }
         public string Origin { get => origin; set => origin = value; }
         public string EcologicalFootprint { get => ecologicalFootprint; set => ecologicalFootprint = value; }
+        public string StorageLocation { get => storageLocation; set => storageLocation = value; }
+        public string Color { get => color; set => color = value; }
+        public string Format { get => format; set => format = value; }
+        public string Quality { get => quality; set => quality = value; }
+        public string Manager { get => manager; set => manager = value; }
+        public string StorageLocation { get => storageLocation; set => storageLocation = value; }
 
         public Product(int id, string name, string category, string provider, string origin, string ecologicalFootprint)
         {
@@ -29,6 +40,12 @@ namespace TPgit
             Provider = provider;
             Origin = origin;
             EcologicalFootprint = ecologicalFootprint;
+        }
+
+        public string displayAllAttributes()
+        {
+            //modify later....
+            return "All attributes";
         }
     }
 }
